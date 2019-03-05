a676ac3bac8988e44417a8783f97446e40f62969
David Blackmon <davidkblackmon@gmail.com>
2019-03-05 19:21:33 +0000

    code for Part 2 of the tutorial
    extract some types to represent success or failure
    of metaCommand and Statement

--- sqlite.hs
+++ sqlite.hs
@@ -1,21 +1,67 @@
 import System.IO
+import System.Exit
 import Control.Monad
 import Text.Printf
+import Data.List
 
-basic_prompt :: String
-basic_prompt = "db > "
+data MetaCommandResult =
+  MetaCommandExit |
+  MetaCommandSuccess |
+  MetaCommandUnrecognizedCommand String
 
-exit_flag :: String
-exit_flag = ".exit"
-  
-unknown_command_notification :: String -> String
-unknown_command_notification s = printf "Unrecognized command '%s'" s
+data PrepareResult =
+  PrepareSuccess |
+  PrepareUnrecognizedCommand
+
+data StatementType =
+  StatementInsert |
+  StatementSelect |
+  StatementUnrecognized
+
+basicPrompt :: String
+basicPrompt = "db > "
+
+unknownCommandNotification :: String -> String
+unknownCommandNotification s = printf "Unrecognized command '%s'" s
+
+prepareMetaCommand :: String -> MetaCommandResult
+prepareMetaCommand ".exit" = MetaCommandExit
+prepareMetaCommand s = MetaCommandUnrecognizedCommand s
+
+executeMetaCommand :: MetaCommandResult -> IO String
+executeMetaCommand MetaCommandExit = exitWith(ExitSuccess)
+executeMetaCommand (MetaCommandUnrecognizedCommand s) = do
+  return $ unknownCommandNotification s
+
+isMetaCommand :: String -> Bool
+isMetaCommand (x:xs) = x == '.'
+
+prepareStatement :: String -> StatementType
+prepareStatement input
+  | isPrefixOf "insert" input = StatementInsert
+  | isPrefixOf "select" input = StatementSelect
+  | otherwise = StatementUnrecognized
+
+--return IO String for now.  this will eventually have side effects
+executeStatement :: StatementType -> IO String
+executeStatement StatementSelect = do
+  return "This is where we would do an select"
+executeStatement StatementInsert = do
+  return "This is where we would do an insert"
+executeStatement StatementUnrecognized = do
+  return "Unrecognized Statement"
+
+processInput :: String -> IO String
+processInput input
+  | isMetaCommand input = executeMetaCommand $ prepareMetaCommand input
+  | otherwise = executeStatement $ prepareStatement input
 
 main :: IO ()
 main = do
-  putStr $ basic_prompt
+  putStr $ basicPrompt
   hFlush stdout
   input <- getLine
-  unless (input == exit_flag) $ do
-    putStrLn $ unknown_command_notification input
-    main
+  result <- processInput input
+  putStrLn result
+  main
+
